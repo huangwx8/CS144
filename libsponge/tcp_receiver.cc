0cc49c4671538cf7cd513cb7d1517266186fffab
huangwx8 <1017417793@qq.com>
2022-02-06 18:21:02 -0800

    实现tcp_receiver，通过check_lab2，未处理异常情况。

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -11,9 +11,48 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 using namespace std;
 
 void TCPReceiver::segment_received(const TCPSegment &seg) {
-    DUMMY_CODE(seg);
+    uint64_t index = 0;
+
+    if (!_isn.has_value()) { // LISTEN
+        if (seg.header().syn) { // LISTEN -> SYN_RECV
+            _isn = seg.header().seqno;
+            index = 0;
+        }
+        else {
+            // error();
+        }
+    }
+    else if (_isn.has_value() && !stream_out().input_ended()) { // SYN_RECV
+        index = unwrap(seg.header().seqno, _isn.value(), _last_reassembled) - 1;
+    }
+    else if (stream_out().input_ended()) { // FIN_RECV
+        index = unwrap(seg.header().seqno, _isn.value(), _last_reassembled) - 1;
+    }
+    else {
+        // error();
+    }
+    
+    if (_isn.has_value()) {
+        _reassembler.push_substring(seg.payload().copy(), index, seg.header().fin);
+        _last_reassembled = index;
+    }
+    
 }
 
-optional<WrappingInt32> TCPReceiver::ackno() const { return {}; }
+optional<WrappingInt32> TCPReceiver::ackno() const {
+    if (!_isn.has_value()) { // LISTEN
+        return {};
+    }
+    if (_isn.has_value() && !stream_out().input_ended()) { // SYN_RECV
+        return wrap(stream_out().bytes_written() + 1, _isn.value());
+    }
+    else if (stream_out().input_ended()) { // FIN_RECV
+        return wrap(stream_out().bytes_written() + 2, _isn.value());
+    }
+    else {
+        // error();
+    }
+    return {};
+}
 
-size_t TCPReceiver::window_size() const { return {}; }
+size_t TCPReceiver::window_size() const { return _capacity - stream_out().buffer_size(); }
--- libsponge/tcp_receiver.hh
+++ libsponge/tcp_receiver.hh
@@ -20,12 +20,16 @@ class TCPReceiver {
     //! The maximum number of bytes we'll store.
     size_t _capacity;
 
+    std::optional<WrappingInt32> _isn;
+
+    uint64_t _last_reassembled;
+
   public:
     //! \brief Construct a TCP receiver
     //!
     //! \param capacity the maximum number of bytes that the receiver will
     //!                 store in its buffers at any give time.
-    TCPReceiver(const size_t capacity) : _reassembler(capacity), _capacity(capacity) {}
+    TCPReceiver(const size_t capacity) : _reassembler(capacity), _capacity(capacity), _isn(), _last_reassembled(0) {}
 
     //! \name Accessors to provide feedback to the remote TCPSender
     //!@{
